5458033af0c4151e7c278ba8032b73241b5a268d
mtconley <matthewT.conley@gmail.com>
2015-08-04 00:10:45 -0700

    initial commit

---
+++ ProcessFlow.py
@@ -0,0 +1,63 @@
+class Node(object):
+    def __init__(self, label=None, **attrs):
+        self.label = label
+        self.id = label
+        for attr in ['size', 'color', 'font', 'fontcolor', 'fontsize']:
+            setattr(self, attr, attrs.get(attr, None))
+        self._set_shape()
+        
+class Condition(Node):
+    def _set_shape(self):
+        self.shape = 'diamond'
+        self.style = 'filled'
+        self.fillcolor = 'purple'
+        self.fontcolor = 'white'
+        
+class Input(Node):
+    def _set_shape(self):
+        self.shape = 'parallelogram'
+        self.style = 'filled'
+        self.fillcolor = 'blue'
+        self.fontcolor = 'gold'
+        
+class Process(Node):
+    def _set_shape(self):
+        self.shape = 'box'
+        self.style = 'filled'
+        self.fillcolor = 'cornflowerblue'
+        self.fontcolor = 'black'
+        
+class End(Node):
+    def _set_shape(self):
+        self.shape = 'invtriangle'
+        self.style = 'filled'
+        self.fillcolor = 'blue'
+        self.fontcolor = 'gold'
+        
+class ProcessFlow(object):
+    import pygraphviz as pgv
+    from StringIO import StringIO
+    
+    def __init__(self, label=None):
+        self.G = self.pgv.AGraph(label=label, id=label, directed=True, splines='ortho')
+        
+    def add_edge(self, u, v, label=''):
+        if isinstance(u, Condition):
+            assert  isinstance(label, bool), ValueError(
+            'Parameter, "label", must be boolean for Conditional edges')
+        self._add_node(u)
+        self._add_node(v)
+        self.G.add_edge(u.label, v.label, None, xlabel=label, id=u.label+'_'+v.label)
+        
+    def _add_node(self, node):
+        if node.label not in self.G:
+            attr = {}
+            for key in node.__dict__:
+                if node.__dict__[key] != None:
+                    attr[key] = node.__dict__[key]
+            self.G.add_node(node.label, **attr)
+               
+    def draw(self, format='svg', prog='dot'):
+        output = self.StringIO()
+        self.G.draw(output, format=format, prog=prog)
+        return output.getvalue()
